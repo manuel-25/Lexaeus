c456b137826d45f9f0c83c27459f15bcb46530fc
Manuel-25 <manuelotamendi97@gmail.com>
2022-07-01 18:01:05 -0300

    Api routes and Header.ejs

--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const products = require('../controllers/productApiController')
 
 router.get('/products', products.list)
-router.get('/product/search', products.search)
+router.get('/products/search', products.search)
 router.get('/products/:id', products.show)
 router.post('/products/create', products.store)
 router.delete('/products/delete/:id', products.delete)
