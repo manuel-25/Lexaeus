c258a6af64b68d8d18716ec5bb5b99af5461b9e5
Manuel-25 <manuelotamendi97@gmail.com>
2022-08-09 18:14:28 -0300

    Index Updated Banner and Categories cards (Roulette)

--- public/js/indexRoulette.js
+++ public/js/indexRoulette.js
@@ -1,13 +1,18 @@
 window.onload = function() {
     let imgBanner = document.querySelector(".banner")
-    
-    const images = ["/img/banner-3.jpg", "/img/Yellow Abstract Summer Sale Banner.png"]
+    let leftArrow = document.querySelector(".fa-chevron-left")
+    let rightArrow = document.querySelector(".fa-chevron-right")
+    const images = ["/img/modern green coming soon (Banner (Landscape)).png", "/img/Yellow Abstract Summer Sale Banner.png", "/img/Lexaeus - Banner (Calzado OFF).png"]
+
+
     let interval = 5000
     let index = 0
+    imagesLength = images.length - 1
     imgBanner.src = images[index]
 
+
     let rouletteFunction = function() {
-        if(index === (images.length - 1)) {
+        if(index === imagesLength) {
             index = 0
         } else {
             index += 1
@@ -15,5 +20,39 @@ window.onload = function() {
         imgBanner.src = images[index]
     }
 
-    window.setInterval(rouletteFunction, interval)
+    let rouletteInterval = window.setInterval(rouletteFunction, interval)
+
+    leftArrow.addEventListener('click', function() {
+        console.log(index)
+        if(index != 0) {
+            index --
+            imgBanner.src = images[index]
+            clearInterval(rouletteInterval)
+            rouletteInterval = window.setInterval(rouletteFunction, interval)
+        } else {
+            if(index == 0) {
+                index = imagesLength
+                imgBanner.src = images[index]
+                clearInterval(rouletteInterval)
+                rouletteInterval = window.setInterval(rouletteFunction, interval)
+            }
+        }
+    })
+
+    rightArrow.addEventListener('click', function() {
+        console.log(index)
+        if(index != imagesLength) {
+            index ++
+            imgBanner.src = images[index]
+            clearInterval(rouletteInterval)
+            rouletteInterval = window.setInterval(rouletteFunction, interval)
+        } else {
+            if(index == imagesLength) {
+                index = 0
+                imgBanner.src = images[index]
+                clearInterval(rouletteInterval)
+                rouletteInterval = window.setInterval(rouletteFunction, interval)
+            }
+        }
+    })
 }
